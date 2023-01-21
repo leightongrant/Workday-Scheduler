bd978bf56341cab86f37a0fe06d1dbdab0c37482
Leighton Grant <oneal.grant@outlook.com>
2023-01-21 20:38:31 +0000

    adds comments

--- script.js
+++ script.js
@@ -9,17 +9,29 @@ const scheduler = {
         return moment(currentMonthString, "YYYY-MM").daysInMonth();
     },
     addToSchedule (element) {
+        // Event delegation
         element.on('click', 'i', function () {
+            // Gets save button
             const currentTime = $(this).parent().parent().children('p.hour').text();
+            // Gets textarea
             const textIn = $(this).parent().parent().children('textarea').val();
+
+            // Check if items already in local storage
 
             if (JSON.parse(localStorage.getItem('items') === null)) {
                 const items = {};
-                items[currentTime] = textIn;
+                // creates and object 
+                for (let i = 0; i < 24; i++) {
+                    let hour = moment().hour(0 + i).format('h A');
+                    items[hour] = '';
+                }
+                // Add item to storage
+                items[currentTime] = textIn.trim();
                 localStorage.setItem('items', JSON.stringify(items));
             } else {
+                // Get items from storage if already exist and adds new item
                 const items = JSON.parse(localStorage.getItem('items'));
-                items[currentTime] = textIn;
+                items[currentTime] = textIn.trim();
                 localStorage.setItem('items', JSON.stringify(items));
             }
         });
@@ -28,28 +40,60 @@ const scheduler = {
 
     },
     renderSchedule () {
-
+        // If no data in local storage render blank schedule
         if (JSON.parse(localStorage.getItem('items') === null)) {
-            console.log('No items in storage');
+            this.createElements();
+            let output = '';
+            for (let i = 0; i < 3; i++) {
+                let hour = moment().hour(0 + i).format('h A');
+                output += `<div class="row">`;
+                output += `<div class="col-12 d-flex description">`;
+                output += `<p class="hour">${hour}</p>`;
+                output += `<textarea type="text" class="past"></textarea>`;
+                output += `<div class="saveBtn d-flex align-items-center justify-content-center">`;
+                output += `<i class="fas fa-save" id="save-${i}"></i></div>`;
+                output += `</div>`;
+                output += `</div>`;
+
+                $(output).appendTo(cont);
+            }
+
         } else {
+            // Render data from local storage
             const items = JSON.parse(localStorage.getItem('items'));
-            console.log(items);
-            // TODO: build elements to render output
+
+            let output = '';
+            for (let i = 0; i < 3; i++) {
+
+                let hour = moment().hour(0 + i).format('h A');
+
+                output += `<div class="row">`;
+                output += `<div class="col-12 d-flex description">`;
+                output += `<p class="hour">${hour}</p>`;
+                output += `<textarea type="text" class="past">${items[hour]}</textarea>`;
+                output += `<div class="saveBtn d-flex align-items-center justify-content-center">`;
+                output += `<i class="fas fa-save" id="save-${i}"></i></div>`;
+                output += `</div>`;
+                output += `</div>`;
+
+            }
+
+            $(output).appendTo(cont);
         }
-
-
     }
 };
 
 // Capture HTML Elements using JQuery
 const currentDayDisplay = $('#currentDay');
-const save = $('.description');
+const save = $('.calendar');
+const cont = $('.container');
 
 
 
 // Render Page
 $(function () {
     scheduler.getCurrentDay(currentDayDisplay, 'MMMM Do, YYYY');
-    scheduler.addToSchedule(save);
     scheduler.renderSchedule();
+    scheduler.addToSchedule(save);
+
 });
